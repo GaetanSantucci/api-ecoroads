fd0b95d2691f0e6d9019cff7def4bfa3c641dc23
GaetanSantucci <99250736+GaetanSantucci@users.noreply.github.com>
2022-08-08 12:08:42 +0200

    :boom: update migration with notice domain constraint

---
+++ migration/deploy/05.alter_domain.sql
@@ -0,0 +1,10 @@
+ALTER DOMAIN postal_code_fr 
+ADD CONSTRAINT "Code postal non valide, merci de renseigner un code situé en France métropolitaine" 
+CHECK (
+    -- 01000 à 09999
+    VALUE ~ '0[1-9]\d{3}' 
+    -- 10000 à 89999
+    OR VALUE ~ '[1-8]\d{4}' 
+    -- les numéros commençant par 9 en métropole
+    OR VALUE ~ '9[0-6]\d{3}'
+);
