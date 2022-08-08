c3db83f7c6288712454639c8a662d8c4db49bc1f
GaetanSantucci <99250736+GaetanSantucci@users.noreply.github.com>
2022-08-08 11:45:08 +0200

    :bug: fix bug

--- app/router/road.js
+++ app/router/road.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 const router = Router();
+
 import { fetchAllRoads } from '../controller/roadController.js';
 import { authenticateToken } from "../middleware/auth.js";
 
